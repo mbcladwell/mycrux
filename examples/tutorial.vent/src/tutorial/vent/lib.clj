c621f625ad0a58334a3e769aa1d912910519574c
mbcladwell <mbcladwell@stihie.net>
2019-10-31 15:14:32 -0400

    working on vent

--- examples/tutorial.vent/src/tutorial/vent/lib.clj
+++ examples/tutorial.vent/src/tutorial/vent/lib.clj
@@ -2,30 +2,30 @@
   (:require
     [tutorial.vent.db :as db]))
 
-(defn favorites
-  []
-  [{:id "1"
-    :text "A favorited tweet"
-    :author {:name "John smith"}
-    :username "john_smith"
-    :favorite? true}
-   {:id "2"
-    :text "Another favorite tweet"
-    :author {:name "Jane Smith"}
-    :username "jane_smith"
-    :favorite? true}])
+;; (defn favorites
+;;   []
+;;   [{:id "1"
+;;     :text "A favorited tweet"
+;;     :author {:name "John smith"}
+;;     :username "john_smith"
+;;     :favorite? true}
+;;    {:id "2"
+;;     :text "Another favorite tweet"
+;;     :author {:name "Jane Smith"}
+;;     :username "jane_smith"
+;;     :favorite? true}])
+
+
 
 (defn all
   []
-  [{:id "1"
-    :text "A hardcoded tweet"
-    :author {:name "John Smith"}
-    :username "john_smith"
-    :favorite? true}
-   {:id "2"
-    :text "A second hardcoded tweet"
-    :author {:name "Jane Smith"}
-    :username "jane_smith"}])
+  (db/read) )
+
+(defn favorites
+  ;;supposed to be anonymous??
+  []
+  (filter :favorite?  (get (all) :vents)))
+
 
 (defn followers
   [{:keys [username]}]
@@ -49,12 +49,25 @@
   []
   (str (java.util.UUID/randomUUID)))
 
-(defn add-vent
-  [{:keys [text username]}]
-  (println username
-           "is venting about" text
-           "with id" (generate-id)))
+;; (defn add-vent
+;;   [{:keys [text username]}]
+;;   (println username
+;;            "is venting about" text
+;;            "with id" (generate-id)))
+
+ (defn add-vent
+   [{:keys [text]}]
+ (db/store  (update (all) :vents conj  {:id (generate-id) :username "developer"  :text text})
+   ))
+
 
 (defn toggle-follow
   [{:keys [to-follow username]}]
-  (println username "is follwoing or unfollowing" to-follow))
+  (println username "is following or unfollowing" to-follow))
+
+(defn add-author
+  []
+  (let [all-users (get (all) :users)
+        all-vents (get (all) :vents) ]
+    (map #(assoc % :author (get (get all-users  (get % :username)) :name) )  all-vents)))
+
